a93bf213bbc790f96624ef802549c1a8774c40fa
shanghanrun <idim7@naver.com>
2024-03-07 21:19:42 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import About from './page/About';
 import Home from './page/Home';
+import LoginPage from './page/LoginPage';
 import ProductDetail from './page/ProductDetail';
 import Products from './page/Products';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import UserPage from './page/UserPage';
+import {useState} from 'react'
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false)
   const PrivateRoute =()=>{
-    return 
+    return authenticate ==true ?<UserPage /> : <Navigate to="/login" />
   }
   return (
     <div >
@@ -16,6 +20,8 @@ function App() {
         <Route path="/about" element={<About />}/>
         <Route path="/products" element={<Products />}/>
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/user' element={<PrivateRoute />} />
       </Routes>
     </div>
   );
