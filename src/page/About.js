d44b9f03a92fc52ae51dae9c31bfdd32ab155234
shanghanrun <idim7@naver.com>
2024-03-08 15:57:08 +0900

    .

--- src/page/About.js
+++ src/page/About.js
@@ -4,14 +4,14 @@ import { useNavigate } from 'react-router-dom'
 
 const About = () => {
 	const navigate = useNavigate(); // navigate함수를 리턴해 준다.
-	function goToHome(){
+	function goHome(){
 		navigate('/')
 	}
   return (
 	<div>
 	  <h1>About Page</h1>
 	  <Link to="/">Home으로</Link>
-	  <button onClick={goToHome}>Home</button>
+	  <button onClick={goHome}>Home</button>
 	</div>
   )
 }
